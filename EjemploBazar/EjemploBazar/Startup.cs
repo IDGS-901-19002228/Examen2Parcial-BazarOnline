83e5fb3d404ecbe4424ddc635bfb0e2a8047f058
IDGS-901-19002228 <19002228@alumnos.utleon.edu.mx>
2023-11-14 13:59:17 -0600

    Examen 2 Parcial

---
+++ EjemploBazar/EjemploBazar/Startup.cs
@@ -0,0 +1,41 @@
+﻿using EjemploBazar;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EjemploBazar
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                var frontendURL = Configuration.GetValue<string>("frontend_url");
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            services.AddControllers();
+        }
+
+
+
+
+        public void Configure(IApplicationBuilder app, IHostApplicationLifetime lifetime)
+        {
+
+        }
+    }
+}
